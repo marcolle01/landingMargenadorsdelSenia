5b8754e67e37dabf4e075c7895191991984bae98
Marc-Olle <marc.olle01@gmail.com>
2024-10-28 13:23:03 +0100

    inicializar

---
+++ client/src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className='pt-9 pb-3 text-center'>
+                <p>&copy; Margenadors del Sénia</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
